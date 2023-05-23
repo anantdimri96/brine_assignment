014edf5433934ff6867e365e40ed891ecd54537e
Anant Dimri <anantdimri@Anants-MacBook-Air.local>
2023-05-23 16:45:30 +0530

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  def render_success(message = 'Success')
-    render json: { success: true, message: }
+  def render_success(message = 'Success', data=[])
+    render json: { success: true, message: message, data: data}
   end
 
   def render_failure(message = 'Failure', status = :unprocessable_entity)
-    render json: { success: false, message: }, status:
+    render json: { success: false, message: message}, status: status
   end
 end
